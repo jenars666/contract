6ffc1bf92f6f613098074195328841cd49fb21ce
Selva Jenars A <selva0jenars@gmail.com>
2026-03-18 04:34:32 +0530

    SmartPatch: Automated security fix for contract_patched.sol

---
+++ contract_patched.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+contract VulnerableBank {
+
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint amount = balances[msg.sender];
+
+        require(amount > 0, "No balance");
+
+        balances[msg.sender] = 0;
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success, "Transfer failed");
+
+
+    }
+}
